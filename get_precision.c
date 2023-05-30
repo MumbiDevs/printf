fbc85d44cbba5f12747ebdd67ce03e3f53764470
ruthosoro <129623587+ruthosoro@users.noreply.github.com>
2023-05-30 21:04:32 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -4,6 +4,7 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ * authors: Rachael and Ruth
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
